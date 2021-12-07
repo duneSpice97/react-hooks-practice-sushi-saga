3f22abe2f8f8b456fe4885860c3985671ab8556b
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2021-12-07 09:27:45 -0700

    cat component with app callbacks

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { render } from "react-dom";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushi, setSushi] = useState([]);
+  const [fourMeals, setfourMeals] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        setSushi(data);
+        setfourMeals([...data.slice(0, 4)]);
+        setCurrentIndex(currentIndex + 4);
+      });
+  }, []);
+
+  function showMoreSushi() {
+    setCurrentIndex(currentIndex + 4);
+    setfourMeals(sushi.slice(currentIndex, currentIndex + 4));
+  }
+
+  function eatDatSushi(image) {
+    console.log('click')
+    console.log(image)
+    // console.log(fourMeals)
+    image="http://www.clker.com/cliparts/9/9/f/0/1194984076762834840small_plate.svg.hi.png"
+
+    // const removeMeal = fourMeals.filter((oneSushiImage) => oneSushiImage.img_url !== image);
+    // setfourMeals(removeMeal);
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer
+        sushi={fourMeals}
+        showMoreSushi={showMoreSushi}
+        eatDatSushi={eatDatSushi}
+      />
       <Table />
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,25 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi.js"
 
-function SushiContainer(props) {
+function SushiContainer({sushi, showMoreSushi, eatDatSushi}) {
+  
+    const renderEachSushi = sushi.map((eachSushi) => {
+      return (
+        <Sushi
+          key={eachSushi.id}
+          image={eachSushi.img_url}
+          name={eachSushi.name}
+          price={eachSushi.price}
+          eatDatSushi={eatDatSushi}
+        />
+      );
+    });
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {renderEachSushi}
+      <MoreButton showMoreSushi={showMoreSushi} />
     </div>
   );
 }
